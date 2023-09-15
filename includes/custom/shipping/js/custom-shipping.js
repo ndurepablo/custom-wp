9b6339bfc57adc9375d2f24007a3c715581d0fb8
Pablo Ndure <ndurepablo@gmail.com>
2023-09-15 18:53:31 -0300

    Actualizacion de estructura objeto de barrios

--- includes/custom/shipping/js/custom-shipping.js
+++ includes/custom/shipping/js/custom-shipping.js
@@ -1,5 +1,6 @@
-const hoods = {
-    "caba": {
+const zones = {
+    "gba": {
+        "caba": {
         "label": "Ciudad autónoma de Buenos Aires",
         "hoods": {
             "1": {
@@ -48,24 +49,59 @@ const hoods = {
     },
     "zona_sur": {
         "label": "Zona Sur",
+            "hoods": {
+                "1": {
+                    "name": "Wilde",
+                    "slug": "hood_wilde",
+                    "id": "datepicker-palermo",
+                    "calculateDisabledDate": 1,
+                    "rangeDays": 5,
+                    "days": [
+                        1,
+                        3,
+                        5
+                    ],
+                    "shippingCost": 2500,
+                    "freeShippingThreshold": 7000
+                },
+                "2": {
+                    "name": "Avellaneda",
+                    "slug": "hood_avellaneda",
+                    "id": "datepicker-caballito",
+                    "calculateDisabledDate": 1,
+                    "rangeDays": 5,
+                    "days": [
+                        1,
+                        2,
+                        3
+                    ],
+                    "shippingCost": 2000,
+                    "freeShippingThreshold": 7000
+                }
+            }
+        }
+    },
+    "country": {
+        "caba": {
+        "label": "Country en zona norte",
         "hoods": {
             "1": {
-                "name": "Wilde",
-                "slug": "hood_wilde",
+                "name": "Palermo",
+                "slug": "hood_palermo",
                 "id": "datepicker-palermo",
                 "calculateDisabledDate": 1,
-                "rangeDays": 5,
+                "rangeDays": 10,
                 "days": [
-                    1,
-                    3,
+                    0,
+                    4,
                     5
                 ],
-                "shippingCost": 1800,
-                "freeShippingThreshold": 7000
+                "shippingCost": 900,
+                "freeShippingThreshold": 4000
             },
             "2": {
-                "name": "Avellaneda",
-                "slug": "hood_avellaneda",
+                "name": "Caballito",
+                "slug": "hood_caballito",
                 "id": "datepicker-caballito",
                 "calculateDisabledDate": 1,
                 "rangeDays": 5,
@@ -74,12 +110,60 @@ const hoods = {
                     2,
                     3
                 ],
-                "shippingCost": 1800,
-                "freeShippingThreshold": 7000
+                "shippingCost": 800,
+                "freeShippingThreshold": 4000
+            },
+            "3": {
+                "name": "Almagro",
+                "slug": "hood_almagro",
+                "id": "datepicker-almagro",
+                "calculateDisabledDate": 1,
+                "rangeDays": 7,
+                "days": [
+                    0,
+                    3,
+                    5
+                ],
+                "shippingCost": 1200,
+                "freeShippingThreshold": 3000
+            }
+        }
+    },
+    "zona_sur": {
+        "label": "Zona Sur",
+            "hoods": {
+                "1": {
+                    "name": "Wilde",
+                    "slug": "hood_wilde",
+                    "id": "datepicker-palermo",
+                    "calculateDisabledDate": 1,
+                    "rangeDays": 5,
+                    "days": [
+                        1,
+                        3,
+                        5
+                    ],
+                    "shippingCost": 2500,
+                    "freeShippingThreshold": 7000
+                },
+                "2": {
+                    "name": "Avellaneda",
+                    "slug": "hood_avellaneda",
+                    "id": "datepicker-caballito",
+                    "calculateDisabledDate": 1,
+                    "rangeDays": 5,
+                    "days": [
+                        1,
+                        2,
+                        3
+                    ],
+                    "shippingCost": 2000,
+                    "freeShippingThreshold": 7000
+                }
             }
         }
     }
-};
+}
 
 jQuery(document).ready(function($){
     $(document.body).on('change', 'input[name="radio_region"]', function(){
@@ -90,12 +174,11 @@ jQuery(document).ready(function($){
         // DECLARACION DE ELEMENTOS NECESARIOS
         let radioButton = $(this).val();
         let customCheckoutField = $('#custom_checkout_field');
-
-        // DOMICIOLIO EN CAPITAL Y GBA SELECCIONADO
-        if (radioButton == 'gba') {
-
-            // CREA EL SELECT CON LOS BARRIOS COMO OPTIONS
-            let gbaSelect = $('<select>', {id: 'gba_zones'});
+        for (let zone in zones){
+            if (zone == radioButton){
+                // CREA EL SELECT CON LOS BARRIOS COMO OPTIONS
+                let hoods = zones[zone];
+                let gbaSelect = $('<select>', {id: 'gba_zones'});
             customCheckoutField.append(gbaSelect);
             for (let key in hoods){ // la key trae la clave principal (caba, zona_sur)
                 gbaSelect.append($('<option>', {
@@ -132,15 +215,25 @@ jQuery(document).ready(function($){
                                 }));
 
                                 //  DATEPICKER
+                                // Declara una variable para mantener una referencia al datepicker
+                                let customDatepicker = null;
                                 hoodSelect.on('change', function() {
+
+
                                     let datePickerElement = $('#custom_shipping_date_field')
                                     const today = new Date();
                                     const nextDay = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
 
                                     datePickerElement.addClass('show');
                                     if (hoodSelect.val() == slug){
-                                        console.log(slug)
-                                        let customDatepicker = jQuery( '#custom_shipping_date' ).datepicker({
+
+                                        // Destruye el datepicker existente si hay uno
+                                        if ($.datepicker._getInst($('#custom_shipping_date')[0])) {
+                                            $('#custom_shipping_date').datepicker('destroy');
+                                        }
+
+                                        customDatepicker = jQuery( '#custom_shipping_date' ).datepicker({
+
                                             dateFormat: 'dd-mm-yy',
                                             minDate: calculateDisabledDate,
                                             maxDate: rangeDays,
@@ -162,33 +255,109 @@ jQuery(document).ready(function($){
                                         });
                                     };
                                 });
-
                             };
                         };
                     };
-
-                // DATEPICKER
-                // hoodSelect.on('change', function(){
-                //     datePickerElement.addClass('show');
-                //
-                //     for (let key in hoods){
-                //         let cityData = hoods[key].hoods;
-                //         if (gbaSelect.val() == key){
-                //             for (let hoodId in cityData){
-                //                 let hood = cityData[hoodId];
-                //                 let {name, slug, id, calculateDisabledDate} = hood;
-                //                 if ()
-                //                 hoodSelect.append($('<option>', {
-                //                 id: slug,
-                //                 value: slug,
-                //                 text: name
-                //                 }));
-                //             };
-                //         };
-                //     };
-                // });
             });
-        };
+
+
+            }
+        }
+
+        // INSTANCIA DE ELIMINACION DE ELEMENTOS
+        // $('#gba_zones').remove();
+        // $('#custom_shipping_cost').remove();
+        //
+        // // DECLARACION DE ELEMENTOS NECESARIOS
+        // let radioButton = $(this).val();
+        // let customCheckoutField = $('#custom_checkout_field');
+        //
+        // // DOMICIOLIO EN CAPITAL Y GBA SELECCIONADO
+        // if (radioButton == 'gba') {
+        //
+            // CREA EL SELECT CON LOS BARRIOS COMO OPTIONS
+            // let gbaSelect = $('<select>', {id: 'gba_zones'});
+            // customCheckoutField.append(gbaSelect);
+            // for (let key in hoods){ // la key trae la clave principal (caba, zona_sur)
+            //     gbaSelect.append($('<option>', {
+            //     id: key,
+            //     value: key,
+            //     text: hoods[key].label
+            //     }));
+            // };
+            //
+            // // ESCUCHAR LOS EVENTOS DE SELECCION EN SELECT DE ZONAS GBA
+            // gbaSelect.on('change', function(){
+            //     // INSTANCIA DE ELIMINACION DE ELEMENTOS
+            //     $('#custom_shipping_cost').remove()
+            //
+            //     // CREACION DEL SELECT QUE CONTIENE LOS BARRIOS
+            //     let hoodSelect = $('<select>', {
+            //         id: 'custom_shipping_cost',
+            //         class: 'form-row-radio'
+            //     });
+            //
+            //     customCheckoutField.append(hoodSelect)
+            //
+            //     // AGREGA LAS OPCIONES AL SELECT DE BARRIOS CON LA DATA DEL OBJETO HOODS
+            //         for (let key in hoods){
+            //             let cityData = hoods[key].hoods;
+            //             if (gbaSelect.val() == key){
+            //                 for (let hoodId in cityData){
+            //                     let hood = cityData[hoodId];
+            //                     let {name, slug, id, calculateDisabledDate, rangeDays, days} = hood;
+            //                     hoodSelect.append($('<option>', {
+            //                     id: slug,
+            //                     value: slug,
+            //                     text: name
+            //                     }));
+            //
+            //                     //  DATEPICKER
+            //                     // Declara una variable para mantener una referencia al datepicker
+            //                     let customDatepicker = null;
+            //                     hoodSelect.on('change', function() {
+            //
+            //
+            //                         let datePickerElement = $('#custom_shipping_date_field')
+            //                         const today = new Date();
+            //                         const nextDay = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
+            //
+            //                         datePickerElement.addClass('show');
+            //                         if (hoodSelect.val() == slug){
+            //
+            //                             // Destruye el datepicker existente si hay uno
+            //                             if ($.datepicker._getInst($('#custom_shipping_date')[0])) {
+            //                                 $('#custom_shipping_date').datepicker('destroy');
+            //                             }
+            //
+            //                             customDatepicker = jQuery( '#custom_shipping_date' ).datepicker({
+            //
+            //                                 dateFormat: 'dd-mm-yy',
+            //                                 minDate: calculateDisabledDate,
+            //                                 maxDate: rangeDays,
+            //
+            //                                 beforeShowDay: function(date) {
+            //                                     if (date.getTime() == nextDay.getTime()) {
+            //                                       // si es el día siguiente, inhabilitarlo
+            //                                       return [false];
+            //                                     };
+            //                                     let day = date.getDay()
+            //
+            //                                     // Comprueba si el día actual está en la lista de días permitidos y es posterior al día siguiente
+            //                                     if (date >= nextDay && days.includes(day)) {
+            //                                         return [true];
+            //                                     } else {
+            //                                         return [false];
+            //                                     }
+            //                                 }
+            //                             });
+            //                         };
+            //                     });
+            //                 };
+            //             };
+            //         };
+            // });
+        // };
     });
 });
 
@@ -211,6 +380,7 @@ jQuery(document).ready(function($){
 // ACTUALIZACION DE COSTO DE ENVIO
 jQuery(document).ready(function($) {
     $('body').on('change', 'select[id="custom_shipping_cost"]', function() {
+        let radioButton = $('input[name="radio_region"]')
         let zoneSelected = $('#gba_zones');
         let customShippingCost = $(this).val();
         $.ajax({
@@ -219,7 +389,8 @@ jQuery(document).ready(function($) {
             data: {
                 action: 'actualizar_costo_envio',
                 custom_shipping_cost: customShippingCost,
-                zone_selected: zoneSelected.val()
+                zone_selected: zoneSelected.val(),
+                radio_selected: radioButton.val()
             },
             success: function(response) {
                 $('body').trigger('update_checkout');
